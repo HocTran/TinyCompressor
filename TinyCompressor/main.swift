ba37ff5079bff9efeb1ebf298976de5e2c0ce575
HocTran <tranhoc78@gmail.com>
2018-06-15 10:29:04 +0700

    Initial Commit

---
+++ TinyCompressor/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TinyCompressor
+//
+//  Created by HocTran on 6/15/18.
+//  Copyright © 2018 Hoc Tran. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
